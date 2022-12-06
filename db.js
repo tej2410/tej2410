e5fb9b57802dc885df8ef41223b58790c543daff
tej2410 <79748228+tej2410@users.noreply.github.com>
2022-12-06 14:45:10 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,19 @@
+{
+  var data = [
+    {
+      text: "GeeksforGeeks",
+      day: "monday",
+      reminder: true
+    },
+    {
+      text: "hospital",
+      day: "firday",
+      reminder: true
+    },
+    {
+      text: "School",
+      day: "firday",
+      reminder: false
+    }
+  ]
+}
